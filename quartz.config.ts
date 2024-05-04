0b319bc6d91c6a32f5a1ab7ee79a75d5ea781388
Opiniated Fox <148921588+OpiniatedFox@users.noreply.github.com>
2024-05-03 23:00:27 -0500

    Update quartz.config.ts
    Changed Page Title to "World of Generica"

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "World of Generica",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
